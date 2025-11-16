70a43906f8df4727ecb72ea97ac0cc1995646344
Филимонова Полина <dxtnrm9ddq@privaterelay.appleid.com>
2025-11-16 15:56:27 +0300

    Add files via upload

---
+++ ЛР1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Создание массива из 5 целых чисел
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // Создание массива без инициализации
+    double nums[3];
+
+    return 0;
+}
---
+++ ЛР1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Создание массива из 5 целых чисел
+        int[] arr = {1, 2, 3, 4, 5};
+
+        // Создание массива фиксированного размера с последующей инициализацией
+        String[] names = new String[3];
+    }
+}
